1591c135ee2073fbee7732555549e72f035de2b5
Alisa <146712422+iggyw1g@users.noreply.github.com>
2025-03-23 16:32:42 +0900

    initial commit

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -86,12 +86,12 @@ export default function Login({ status, canResetPassword }) {
                             href={route('password.request')}
                             className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
-                            Forgot your password?
+                            HELLOHELLOHELLO
                         </Link>
                     )}
 
                     <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
+                        Testing
                     </PrimaryButton>
                 </div>
             </form>
